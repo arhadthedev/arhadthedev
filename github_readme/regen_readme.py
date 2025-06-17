a1ae86a372be556af4e67b500313cbdd58304924
Oleg Iarygin <oleg@arhadthedev.net>
2025-06-17 22:39:05 +0400

    Return the multiline function call commas
    Fix the Ruff linter errors like this:
    
    > github_readme/regen_readme.py:67:54: COM812 [*] Trailing comma missing

--- github_readme/regen_readme.py
+++ github_readme/regen_readme.py
@@ -52,19 +52,19 @@ def _make_contrib_line(contribs: NestedDict[str], match: re.Pattern) -> str:
             lambda: contribs[repo_name].get('commit_count', 0),
             f"{repo_path}/commits?author={contribs['author']}",
             lambda count: 's' if count > 1 else '',
-            '[{count} already merged commit{plural}]({url})'
+            '[{count} already merged commit{plural}]({url})',
         ),
         Contribution(
             lambda: contribs[repo_name].get('pr_count', 0),
             f"{repo_path}/pulls/{contribs['author']}",
             lambda count: 's are' if count > 1 else ' is',
-            '[{count} PR{plural} awaiting merging]({url})'
+            '[{count} PR{plural} awaiting merging]({url})',
         ),
         Contribution(
             lambda: contribs[repo_name].get('issue_count', 0),
             f"{repo_path}/issues?q=is%3Aissue+author%3A{contribs['author']}",
             lambda count: 's' if count > 1 else '',
-            '[{count} reported issue{plural}]({url})'
+            '[{count} reported issue{plural}]({url})',
         ),
     ]
 
